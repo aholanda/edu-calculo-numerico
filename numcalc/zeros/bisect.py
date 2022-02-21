aa8e5070bdd2a32aa29afdddb82c5dae361378cb
Adriano J. Holanda <adriano.holanda@fafram.com.br>
2022-02-21 17:52:00 -0300

    sync with class notes

--- numcalc/zeros/bisect.py
+++ numcalc/zeros/bisect.py
@@ -1,21 +1,18 @@
 from __future__ import division
 import math
-NITER = 40
-def rtbis(f, a, b, xac):
-    f1 = f(a)
-    fm = f(b)
-    if f1*fm >= 0.0:
+N = 40
+def rtbis(f, x1, x2, xac):
+    if f(x1)*f(x2) >= 0.0:
         raise ValueError('Root must be bracketed for bisection.')
-    for i in range(NITER):
-        m = (a+b)/2
-        fm = f(m)
-        if m == 0.0 or (b-a)/2 < xac:
-            return m
-        if f1*fm > 0.0:
-            a = m
-            f1 = fm
+    for i in range(N):
+        xm = (x1+x2)/2
+        y = f(xm)
+        if y == 0.0 or abs(x2-x1)/2 < xac:
+            return xm
+        if f(x1)*y > 0.0:
+            x1 = xm
         else:
-            b = m
+            x2 = xm
 
     raise ValueError('Too many bisections.')
 
@@ -23,5 +20,5 @@ def f(x):
     return math.exp(x) - x -2
 
 if __name__ == '__main__':
-    r = rtbis(f, -2, 2, 0.01)
+    r = rtbis(f, -2, 0, 0.01)
     print('f({})~=0'.format(r))
