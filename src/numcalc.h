0bd6064e426ddba7a9830bbe58ee6ec53178fbc9
Adriano J. Holanda <adriano.holanda@fafram.com.br>
2022-03-04 16:16:32 -0300

    change bisection function name and add regula

--- src/numcalc.h
+++ src/numcalc.h
@@ -1,6 +1,7 @@
 #ifndef __NUMCALC_H__
 #define __NUMCALC_H__
 
-extern float zerobis(float f(float x), float x1, float x2, float xac);
+extern float bisect(float f(float x), float x1, float x2, float xac);
+extern float regula(float f(float x), float x1, float x2, float xac);
 
 #endif
