a544b05999cd45917fa7c622b45863f1d1a535ad
Adriano J. Holanda <adriano.holanda@fafram.com.br>
2022-03-07 10:14:38 -0300

    initial commit

---
+++ src/newton.c
@@ -0,0 +1,18 @@
+#include "numcalc.h"
+#define N 40
+float newton(float fd(float x, float *f, float *df), 
+	     float a, float b, float eps) {
+	int i=0;
+	float f, df, dx, m;
+	m=0.5*(a+b);
+    	while (i<N) {
+		fd(m, &f,&df);
+		dx=f/df;
+		m -= dx;
+        	if ((a-m)*(m-b) < 0.0) ERROR("Jumped out of brackets.");
+		if (fabs(dx) < eps) return m;
+		i++;
+    }
+    ERROR("Limit of %d iterations exceeded.\n", N);
+    return 0; /* Never get here. */
+}
