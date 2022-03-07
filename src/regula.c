12367e9ad83b8da1bd90228b8821312460b778a2
Adriano J. Holanda <adriano.holanda@fafram.com.br>
2022-03-07 11:52:32 -0300

    make fa and fb assignments in one line

--- src/regula.c
+++ src/regula.c
@@ -4,8 +4,7 @@ float regula(float f(float x), float a, float b, float eps) {
 	int i=0;
 	float d, dx, m, fm, fa, fb, t;
 	if (f(a)*f(b) >= 0.0) ERROR("f(a)f(b)<0=false\n");
-	fa = f(a);
-	fb = f(b);
+	fa = f(a), fb = f(b);
 	if (fb < 0.0) t=b, b=a, a=t, t=fb, fb=fa, fa=t;
 	dx=b-a;
     	while (i<N) {
