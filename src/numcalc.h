4aad06d55d0568b981a6d5108171fadeac008fd7
Adriano J. Holanda <adriano.holanda@fafram.com.br>
2022-03-07 11:08:20 -0300

    split definitions into numcalc.h and zero.h

--- src/numcalc.h
+++ src/numcalc.h
@@ -5,17 +5,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "zero.h"
+
 #define ERROR(...) do {\
 		fprintf(stderr, "ERROR: %s.%d: ", \
 			__FILE__, __LINE__); \
 	       	fprintf(stderr, __VA_ARGS__); \
 		exit(1); \
 	} while(0)
-
-/* Zero finding of non-linear functions */
-extern float bisect(float f(float x), float x1, float x2, float xac);
-extern float regula(float f(float x), float x1, float x2, float xac);
-extern float newton(float f(float x, float *f, float *df), 
-		    float x1, float x2, float xac);
 
 #endif
